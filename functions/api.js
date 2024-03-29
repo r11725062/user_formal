201435fc60b0645c47758011b15fe6331747713f
r11725062 <163897811+r11725062@users.noreply.github.com>
2024-03-29 16:13:58 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,13 +1,17 @@
 // functions/api.js
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   const fetch = (await import('node-fetch')).default;
   const { userMessage, chatHistory } = JSON.parse(event.body); // 假设客户端会发送包含聊天历史的请求
   const API_KEY = process.env.MY_API_KEY;
 
+  // 确保从客户端接收到的 chatHistory 是一个数组
+  const validatedChatHistory = Array.isArray(chatHistory) ? chatHistory : [];
+
+
   // 构造 messages 数组，包括系统信息、之前的对话历史和最新的用户消息
-  const messages = [
+  let messages = [
     {role: "system", content: "As a professional and solemn salary negotiation coach, your role is to use your knowledge of salary negotiation, including negotiation strategies, steps, and how to prepare for salary negotiations, to instruct users in 'Traditional Chinese' on all aspects of salary negotiation knowledge and skills. When addressing user questions, you may consult 'salary negotiation.txt,' which includes preparation tips and brief case studies for user reference. Please answer users as thoroughly as possible based on this document and your pre-existing knowledge. Your tone should always remain stern and formal, without any emotion or warmth, to ensure the user perceives you as highly professional and serious.###RULES### Always use 'Traditional Chinese' when responding to users.Do not use Simplified Chinese.Address users with respect, using honorifics such as 'You' and 'Please.'Maintain seriousness at all times, but do not mention 'you are very serious' in your responses.Do not display any friendliness or enthusiasm.Do not mention the document provided to you; do not let users know that you are referencing a document. Respond as concisely as possible to reduce the length of responses.Respond in a list format as much as possible.###EXAMPLES### Regarding your question XXX, I have provided a few XX for your consideration:/n1. XXX/n2. XXX/n3. XXX /nThese are the answers I have provided for you."},
-    ...chatHistory, // 将之前的聊天历史添加到 messages 数组中
+    ...validatedChatHistory, // 将之前的聊天历史添加到 messages 数组中
     {role: "user", content: userMessage},
   ];
 
@@ -30,13 +34,13 @@ exports.handler = async (event, context) => {
   const data = await response.json();
 
   // 将机器人的回应添加到聊天历史中
-  chatHistory.push({role: "assistant", content: data.choices[0].message.content.trim()});
+  validatedChatHistory.push({role: "assistant", content: data.choices[0].message.content.trim()});
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: data.choices[0].message.content.trim(),
-      updatedChatHistory: chatHistory // 返回更新后的聊天历史，以便客户端存储
+      updatedChatHistory: validatedChatHistory // 返回更新后的聊天历史，以便客户端存储
     })
   };
 }
